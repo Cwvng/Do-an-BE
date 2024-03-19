857fd9d28f0d0ae37d5fc7a72fc6ed9a8d400c8b
QuangTran <quangtrancnh@gmail.com>
2024-03-19 15:11:39 +0700

    backend first init

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+import authRoutes from "./routes/auth.routes.js";
+import connectDb from "./db/connectDb.js";
+import messageRoutes from "./routes/message.routes.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
+const app = express();
+const PORT = process.env.PORT || 5000
+
+dotenv.config()
+
+app.use(express.json()) // parse incoming rq with JSON payload (from req.body)
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
+
+app.listen(PORT,()=>{
+    connectDb();
+    console.log(`Server is running on port ${PORT}`)
+})
