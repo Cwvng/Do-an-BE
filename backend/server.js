c7593e88318d447b9c413248b942086da4dba444
QuangTran <quangtrancnh@gmail.com>
2024-03-26 11:32:36 +0700

    init backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-
+import logger from 'morgan'
 import authRoutes from "./routes/auth.routes.js";
 import connectDb from "./db/connectDb.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -11,9 +11,14 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config()
 
+app.use(logger('dev'))
 app.use(express.json()) // parse incoming rq with JSON payload (from req.body)
 app.use(cookieParser())
 
+
+app.use('/',(req,res)=>{
+    return res.status(200).json({message:"Server is running!"})
+})
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
