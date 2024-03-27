de6e1f18c86166d8711dcddd3193c5225dd015b8
QuangTran <quangtrancnh@gmail.com>
2024-03-27 09:39:17 +0700

    config jwt in signup

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,25 +1,37 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 import generateToken from "../utils/generateToken.js";
+import JWT from 'jsonwebtoken';
 
+const encodedToken = (userId) => {
+    return JWT.sign({
+        iss: "Admin",
+        sub: userId,
+        iat: new Date().getTime(),
+        exp: new Date().setDate(new Date().getDate() + 3)
+    }, process.env.JWT_SECRET)
+}
 export const signup = async (req, res) => {
     try {
-        const {firstname,lastname, email, password, confirmPassword, gender} = req.body;
+        const {firstname, lastname, email, password, confirmPassword, gender} = req.body;
 
+        //Validate request
         if (password !== confirmPassword) {
             return res.status(400).json({error: "Password don't match"})
         }
-        const user = await User.findOne({email})
-        if (user) return res.status(400).json({error: "User existed"})
 
-        //HASH PASSWORD
+        const user = await User.findOne({email})
+        if (user) return res.status(403).json({error: "Email already existed"})
+
+        //Hash password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt)
 
-        //
+        //Create default avatar
         const malePic = `https://avatar.iran.liara.run/public/boy?username=${firstname}`
         const femalePic = `https://avatar.iran.liara.run/public/girl?username=${firstname}`
 
+        //Create new user
         const newUser = User({
             firstname,
             lastname,
@@ -29,19 +41,20 @@ export const signup = async (req, res) => {
             profilePic: gender === 'male' ? malePic : femalePic
         })
         if (newUser) {
-            //JWT token
-            await generateToken(newUser._id, res)
             await newUser.save()
 
+            //Encoded a token
+            const token = encodedToken(newUser._id);
+
+            //Response
+            res.setHeader("Authorization", token)
             res.status(201).json({
                 _id: newUser._id,
                 firstname: newUser.firstname,
-                lastname:newUser.lastname,
+                lastname: newUser.lastname,
                 email: newUser.email,
                 profilePic: newUser.profilePic
             })
-        } else {
-            res.status(400).json({error: "Invalid user data"})
         }
 
     } catch (err) {
@@ -59,8 +72,8 @@ export const login = async (req, res) => {
         }
         generateToken(user._id, res);
         res.status(200).json({
-            _id:user._id,
-            fullName:user.fullName,
+            _id: user._id,
+            fullName: user.fullName,
             userName: user.username,
             profilePic: user.profilePic
         })
@@ -70,15 +83,15 @@ export const login = async (req, res) => {
     }
 }
 export const logout = async (req, res) => {
-    try{
-        res.cookie('jwt',"",{maxAge:0});
-        res.status(200).json({message:'Logged out'})
-    }catch (err) {
+    try {
+        res.cookie('jwt', "", {maxAge: 0});
+        res.status(200).json({message: 'Logged out'})
+    } catch (err) {
         console.log("Logout error")
         res.status(500).json({error: err.message})
     }
 }
 
-export const secret =  (req, res)=>{
+export const secret = (req, res) => {
 
 }
