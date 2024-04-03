340a3536dd756047efa921493775b152a2b67041
QuangTran <quangtrancnh@gmail.com>
2024-04-03 22:26:31 +0700

    done get user api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,8 +2,17 @@ import User from '../models/user.model.js'
 
 export const getAllOtherUsers = async (req, res) => {
   try {
-    const loggedUserId = req.user._id
-    const allUsers = await User.find({ _id: { $ne: loggedUserId } })
+    const keyword = req.query.keyword
+            ? {
+                $or: [
+                    {email: {$regex: req.query.keyword, $options: "i"}},
+                    {firstname: {$regex: req.query.keyword, $options: "i"}},
+                    {lastname: {$regex: req.query.keyword, $options: "i"}}
+                ]
+            }
+            : {};
+        const loggedUserId = req.user._id
+    const allUsers = await User.find(keyword).find({ _id: { $ne: loggedUserId } })
     res.status(200).json(allUsers)
   } catch (err) {
     console.log('Error in userController: ', err.message)
