6ae235657a03293655efdab15909708fbb98e96d
QuangTran <quangtrancnh@gmail.com>
2024-04-09 10:52:22 +0700

    fix create chat

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -4,39 +4,39 @@ import Chat from '../models/chat.model.js'
 import User from '../models/user.model.js'
 
 export const accessChat = async (req, res, next) => {
+  const { userId } = req.body
+
+  if (!userId) return next(new ApiError(StatusCodes.BAD_REQUEST, 'User Id not found'))
+
   try {
-    console.log(req.body)
-    const { userId } = req.body
-
-    if (!userId) next(new ApiError(StatusCodes.BAD_REQUEST, 'User Id not found'))
-
+    console.log(req.user._id, userId)
     let isChat = await Chat.find({
       isGroupChat: false,
       $and: [
-        { user: { $elemMatch: { $eq: req.user._id } } },
-        { user: { $elemMatch: { $eq: userId } } }
+        { users: { $elemMatch: { $eq: req.user._id } } },
+        { users: { $elemMatch: { $eq: userId } } }
       ]
     }).populate('users', '-password')
       .populate('latestMessage')
-
+    console.log(isChat)
     isChat = await User.populate(isChat, {
       path: 'latestMessage.sender',
       select: 'firstname lastname email profilePic'
     })
     console.log(isChat)
+
     if (isChat.length > 0) {
       res.send(isChat[0])
     } else {
-      var chatData = {
+      const chatData = {
         chatName: 'sender',
         isGroupChat: false,
         users: [req.user._id, userId]
       }
+      const createdChat = await Chat.create(chatData)
+      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate('users', '-password')
+      res.status(200).send(fullChat)
     }
-    const createdChat = await Chat.create(chatData)
-
-    const fullChat = await Chat.findOne({ _id: createdChat._id }).populate('users', '-password')
-    res.status(200).send(fullChat)
   } catch (err) {
     next(new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, err.message))
   }
