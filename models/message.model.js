ace59db2cc245dd38ed582882f0530d8326a1a3d
QuangTran <quangtrancnh@gmail.com>
2024-04-12 16:37:31 +0700

    add api send message and get message

--- models/message.model.js
+++ models/message.model.js
@@ -1,22 +1,13 @@
 import mongoose from 'mongoose'
 
-const messageSchema = new mongoose.Schema({
-  senderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const messageSchema = new mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat' },
+    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
   },
-  receiverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  message: {
-    type: String,
-    required: true
-  }
-  // createdAt, updatedAt
-}, { timestamps: true })
+  { timestamps: true })
 
 const Message = mongoose.model('Message', messageSchema)
 
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getMessages, sendMessage } from '../controllers/message.controller.js'
+import { getAllMessagesFromChat, sendMessage } from '../controllers/message.controller.js'
 import { handleAuthentication } from '../middleware/passport.js'
 
 const router = express.Router()
-router.post('/send/:id', (req, res, next) => handleAuthentication('jwt', req, res, next), sendMessage)
-router.get('/:id', (req, res, next) => handleAuthentication('jwt', req, res, next), getMessages)
+
+router.post('/', (req, res, next) => handleAuthentication('jwt', req, res, next), sendMessage)
+router.get('/:chatId', (req, res, next) => handleAuthentication('jwt', req, res, next), getAllMessagesFromChat)
+
 export default router
