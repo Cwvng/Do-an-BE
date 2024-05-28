5711fd99500986527b46c82050149e37231dc9b7
QuangTran <quangtrancnh@gmail.com>
2024-05-28 14:37:27 +0700

    fix bug

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import { createMessage, deleteMessage, updateMessage } from '../controllers/message.controller.js'
+import {
+  createMessage,
+  deleteMessage,
+  getMessageImagesList,
+  updateMessage
+} from '../controllers/message.controller.js'
 import { handleAuthentication } from '../middleware/passport.js'
+import uploadCloud from '../config/cloudinary.config.js'
 
 const router = express.Router()
 
-router.post('/', (req, res, next) => handleAuthentication('jwt', req, res, next), createMessage)
+router.post('/', (req, res, next) => handleAuthentication('jwt', req, res, next), uploadCloud.array('images', 3), createMessage)
 router.delete('/:id', (req, res, next) => handleAuthentication('jwt', req, res, next), deleteMessage)
 router.patch('/:id', (req, res, next) => handleAuthentication('jwt', req, res, next), updateMessage)
+router.get('/:chatId/images', (req, res, next) => handleAuthentication('jwt', req, res, next), getMessageImagesList)
 
 export default router
